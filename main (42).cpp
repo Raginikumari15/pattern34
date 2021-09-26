307daff15ec27c3c30920543add87735f90d116a
Ragini kumari <88733937+Raginikumari15@users.noreply.github.com>
2021-09-27 02:50:47 +0530

    Add files via upload

---
+++ main (42).cpp
@@ -0,0 +1,25 @@
+#include <iostream>                   //55555
+using namespace std;                   //45555
+int main()                             //34555
+{                                      //23455
+int i = 1;                             //12345 
+int n = 5;
+int k = 5;
+while(i<=5){
+     int j = 1;
+      while(j<= 5){
+          if (j<=i-1){
+              cout<< k;
+              k++;
+          }
+              
+              else{
+              cout<< n;
+              }
+              j++;
+      }
+      cout<< endl;
+      i++;
+       k = 5 -i +1;
+      }
+     }
